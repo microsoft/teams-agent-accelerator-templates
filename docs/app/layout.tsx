bb5a216465aab578da41f9393ab4db4517d4c9f3
Kavin Singh <kavinsingh@microsoft.com>
2025-03-25 02:28:25 -0700

    integrate clarity

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -8,6 +8,7 @@ import {
 import { useState } from 'react';
 import useStyles from './layout.styles';
 import { ThemeContext } from './contexts/ThemeContext';
+import ClarityScript from './clarity';
 
 export default function RootLayout({
   children,
@@ -17,11 +18,13 @@ export default function RootLayout({
   const classes = useStyles();
   const [isDark, setIsDark] = useState(false);
 
+  const isProduction = process.env.NEXT_PUBLIC_BASE_PATH ? true : false;
   const toggleTheme = () => setIsDark(!isDark);
 
   return (
     <html lang="en">
       <body className={classes.root}>
+        { isProduction ?? <ClarityScript /> }
         <ThemeContext.Provider value={{ isDark, toggleTheme }}>
           <FluentProvider theme={isDark ? webDarkTheme : webLightTheme}>
             <main className={classes.main}>{children}</main>
