7421ce3649aae8e210aa35f6f212ee3c1e2759a5
lilydu <lilydu+odspmdb@microsoft.com>
2025-04-03 11:50:07 -0700

    remove extra parentheses

--- dotnet/dex-agent/GitHubService.cs
+++ dotnet/dex-agent/GitHubService.cs
@@ -103,7 +103,7 @@ namespace DexAgent
                             },
                             cancellationToken);
                     }
-                });
+                };
             }
         }
 
